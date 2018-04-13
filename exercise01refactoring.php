2e26215dd56d1920bf6fcac79117603e1c6e043f
ToBeAmped <martinja@oregonstate.edu>
2018-04-12 22:42:45 -0700

    Add exercise01refactoring.php

---
+++ exercise01refactoring.php
@@ -0,0 +1,49 @@
+<?php
+class IceBoxRefrigerator {
+    public $height, $width, $depth;
+    public function add($beverage) { }
+}
+
+class BrokenRefrigerator {
+    public $height, $width, $depth;
+    public function add($beverage) { }
+}
+
+class RootBeer { }
+class Kombucha { }
+
+class Kitchen {
+
+    private $refrigerator;
+
+    public function __construct() {
+        $this->refrigerator = new IceBoxRefrigerator();
+    }
+
+    public function addBeerToRefrigerator() {
+        $this->refrigerator->add( new RootBeer() );
+    }
+
+    public function __toString() {
+        $fridgeVolume = ($this->refrigerator->height * $this->refrigerator->width * $this->refrigerator->depth);
+        $fridgeVolumeGallons = $fridgeVolume * 7.48052;
+        return "Kitchen. Fridge volume = " . $fridgeVolumeGallons . " gallons";
+    }
+
+}
+
+// Current usage example:
+
+$kitchen = new Kitchen();
+$kitchen->addBeerToRefrigerator();
+echo (string)$kitchen;
+
+// $kitchen has an IceBoxRefrigerator with a RootBeer in it.
+
+// Problem: you can't create a kitchen with a different fridge, and you can't
+//          add any beverage other than beer.
+
+// Goal: Create a kitchen with a BrokenRefrigerator, and add Kombucha to it.
+
+?>
+
